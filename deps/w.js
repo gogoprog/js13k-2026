1ed1d6fd0b864d4a4d7dfb4d4c5d47d9c69aa9d8
gogoprog <gogoprog@gmail.com>
2026-08-24 21:36:04 +0200

    wip

--- deps/w.js
+++ deps/w.js
@@ -85,54 +85,24 @@ W = {
       
       `#version 300 es
       precision highp float;                  // Set default float precision
-      // in vec4 v_pos, v_col, v_uv, v_normal;   // Varyings received from the vertex shader: position, color, texture coordinates, normal (if any)
-      // uniform vec3 light;                     // Uniform: light direction, smooth normals enabled
-      // uniform vec4 o;                         // options [smooth, shading enabled, ambient, mix]
-      // uniform vec2 uv_scale;
-      // uniform sampler2D sampler;              // Uniform: 2D texture
-      // out vec4 c;                             // Output: final fragment color
+      in vec4 v_pos, v_col, v_uv, v_normal;   // Varyings received from the vertex shader: position, color, texture coordinates, normal (if any)
+      uniform vec3 light;                     // Uniform: light direction, smooth normals enabled
+      uniform vec4 o;                         // options [smooth, shading enabled, ambient, mix]
+      uniform vec2 uv_scale;
+      uniform sampler2D sampler;              // Uniform: 2D texture
+      out vec4 c;                             // Output: final fragment color
 
       // // The code below displays colored / textured / shaded fragments
-      // void main() {
-      //   c = mix(texture(sampler, v_uv.xy * uv_scale), v_col, o.w);  // base color (mix of texture and rgba)
-      //   if(o.y > 0.){                                    // if lighting/shading is enabled:
-      //     c = vec4(                                       // output = vec4(base color RGB * (directional shading + ambient light)), base color Alpha
-      //       c.rgb * (max(0., dot(normalize(light), -normalize(       // Directional shading: compute dot product of light direction and normal (0 if negative)
-      //         o.x > 0.                                   // if smooth shading is enabled:
-      //         ? vec3(v_normal.xyz)                        // use smooth normals passed as varyinghighp 
-      //         : cross(dFdx(v_pos.xyz), dFdy(v_pos.xyz))   // else, compute flat normal by making a cross-product with the current fragment and its x/y neighbours
-      //       )))
-      //       + o.z),                                      // add ambient light passed as uniform
-      //       c.a                                           // use base color's alpha
-      //     );
-      //   }
-      // }
-      //
-in vec4 v_pos, v_col, v_uv, v_normal;
-uniform vec3 light;
-uniform vec4 o;
-uniform sampler2D sampler;
-out vec4 c;
-
-void main() {
-  vec3 n = normalize(cross(dFdx(v_pos.xyz), dFdy(v_pos.xyz)));
-  c = vec4(n * 0.5 + 0.5, 1.0);
-  // return;
-  c = mix(texture(sampler, v_uv.xy), v_col, o.w);
-  if (o.y > 0.0) {
-    vec3 n;
-    if (o.x > 0.0) {
-      n = v_normal.xyz;
-    } else {
-      n = cross(dFdx(v_pos.xyz), dFdy(v_pos.xyz));
-    }
-    // c = vec4(light, 1.0);
-    // c = vec4(normalize(n)*0.5 + 0.5, 1.0);
-    // return ;
-    float diffuse = max(0., dot(light, -normalize(n)));
-    c = vec4(c.rgb * (diffuse + o.z), c.a);
-  }
-}
+      void main() {
+        c = mix(texture(sampler, v_uv.xy * uv_scale), v_col, o.w);  // base color (mix of texture and rgba)
+          c = vec4(                                       // output = vec4(base color RGB * (directional shading + ambient light)), base color Alpha
+            c.rgb * (max(0., dot(normalize(light), -normalize(       // Directional shading: compute dot product of light direction and normal (0 if negative)
+               vec3(v_normal.xyz)                        // use smooth normals passed as varyinghighp 
+            )))
+            + o.z),                                      // add ambient light passed as uniform
+            c.a                                           // use base color's alpha
+          );
+      }
     `
     );
     
@@ -382,7 +352,7 @@ void main() {
       }
       
       // Set the normals buffer
-      if ((object.s || model.customNormals) && model.normalsBuffer) {
+      if ( model.normalsBuffer) {
         W.gl.bindBuffer(34962 /* ARRAY_BUFFER */, model.normalsBuffer);
         W.gl.vertexAttribPointer(buffer = W.gl.getAttribLocation(W.program, 'normal'), 3, 5126 /* FLOAT */, false, 0, 0);
         W.gl.enableVertexAttribArray(buffer);
